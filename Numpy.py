8d147ab3771e37cd2075b978530cc7356df19488
fayzi-dev <fayzi.dev@gmail.com>
2024-08-16 09:21:37 +0330

    برش آرایه

--- Numpy.py
+++ Numpy.py
@@ -3,8 +3,6 @@
 # Install Numpy:Install it using this command:
 # pip
 # pip3
-
-
     pip install numpy
     pip3 install numpy
         
@@ -13,43 +11,38 @@
 
     import numpy as np
     np.__version__
-     
-'1.26.4'
-
 
 # Create a new arrray by Numpy
-
-
     arr = np.array([1, 2, 3, 4])
     # print(arr)
     print(arr.shape)
-        
-    (4,)
 
 
 # Use a tuple to create a NumPy array:
 
     arr_tuple = np.array((1 ,2 ,3 , 4, 5, 6))
     print(arr_tuple)
-        
-    array([1, 2, 3, 4, 5, 6])
+ 
 # ایجاد آرایه دو بعدی
 
     arr_2_D = np.array([[10,  20, 30],[200, 300, 400]])
     print(arr_2_D)
         
-    [[ 10  20  30]
-    [200 300 400]]
 # (shape) برای نمایش تعداد بعد آرایه
 
     print(arr_2_D.shape)
-        
-    (2, 3)
-
+ 
 
 # دسترسی به عناصر آرایه ها 
-    arr = np.array([1, 2, 3, 4])
+   arr = np.array([1, 2, 3, 4])
 
-    print(arr[0])
+   print(arr[0])
 
-    1
+# Slicing arrays برش آرایه ها
+# Slicing in python means taking elements from one given index to another given index.
+
+# We pass slice instead of index like this: [start:end].
+
+# We can also define the step, like this: [start:end:step].
+    arr_b = np.array([[1, 2, 3],[6, 7, 8],[9, 10, 11]])
+    print(arr_b[1:2])
