dd9ca3ae86338a144d65130c5707bb502e7dfefc
fayzi-dev <fayzi.dev@gmail.com>
2024-08-16 09:56:36 +0330

    reshape and lterate

--- Numpy.py
+++ Numpy.py
@@ -70,3 +70,38 @@
 # Create an array with data type String:
     arr_c = np.array(['ali', 'mohammad','hasan'],dtype='S')
     print(arr_c.dtype)
+
+# Reshape Numpy
+# Convert the following 1-D array with 12 elements into a 2-D array.
+
+# The outermost dimension will have 4 arrays, each with 3 elements:
+    arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+    newarr = arr.reshape(4, 3)
+
+    print(newarr)
+
+# Convert the following 1-D array with 12 elements into a 3-D array.
+
+# The outermost dimension will have 2 arrays that contains 3 arrays, each with 2 elements:
+    arr = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+
+    newarr = arr.reshape(2, 3, 2)
+   
+    print(newarr)
+
+
+# Check if the returned array is a copy or a view:
+
+    import numpy as np
+
+    arr = np.array([1, 2, 3, 4, 5, 6, 7, 8])
+
+    arr.reshape(2, 4).base
+
+# Iterate on the elements of the following 2-D array:
+
+    arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+    for x in arr:
+    print(x)
