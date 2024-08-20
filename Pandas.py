e89863c20b1e2080233c5a206a4b37b62fec9f53
fayzi-dev <fayzi.dev@gmail.com>
2024-08-20 14:14:25 +0330

    Analiz data whit pandas function

--- Pandas.py
+++ Pandas.py
@@ -48,3 +48,10 @@
 
     # Select rows by condition
     print(df[df['Age'] > 25])
+
+
+# Pandas - Analyzing DataFrames 
+
+    print(df.head(10)) #Get a quick overview by printing the first 10 rows of the DataFrame
+    print(df.tail()) #Print the last 5 rows of the DataFrame
+    print(df.info()) #Print information about the data
