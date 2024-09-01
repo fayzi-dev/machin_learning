cdc42180ba58d05c9fde43d8c17263355d21b4c8
Mohammad Fayzi <fayzi.dev@gmail.com>
2024-09-01 21:54:03 +0330

    Machin learnings 'Linear model Regression on Tips Datasets'

--- ml_algoritm.py
+++ ml_algoritm.py
@@ -1,4 +1,4 @@
-# Data set :
+# Linear Regression :
 
 
 import numpy as np
@@ -6,10 +6,43 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import seaborn as sns
 
+data = pd.read_csv("/home/m-fayzi/Desktop/machin_learning/tips.csv")
 
-df = sns.load_dataset("penguins")
-# print(df)
+# print(data.head())
+# print(data.info())
+# print(data.shape)
+# print(data.describe())
+# print(data.sample(10))
+# print(data.groupby("day").count())
 
-sns.pairplot(df, hue="species")
-plt.show()
-# print(x)
+# df2 = data.groupby('day').sum()
+# df2.drop(['smoker','sex','time'], inplace=True, axis=1)
+# df2['percent'] = df2['tip'] / df2['total_bill'] * 100
+# print(df2)
+
+
+
+# df3 = data.groupby('smoker').sum()
+# df3['percent'] =df3['tip'] / df3['total_bill'] * 100
+# print(df3)
+
+
+
+# df4 = data.groupby(['day','size']).sum()
+# df4['percent'] = df4['tip'] / df4['total_bill'] * 100
+# print(df4)
+
+
+# sns.catplot(x='day', kind='count', data=data, color='green')
+# plt.show()
+
+# sns.catplot(x='day', hue='size', kind='count', data=data)
+# plt.show()
+
+data.replace({'sex':{'Male': 0 ,'Female': 1 }, 'smoker': {'No': 0 , 'Yes': 1}}, inplace=True)
+
+# print(data.head())
+days = pd.get_dummies(data['day'], dtype=int)
+data = pd.concat([data, days], axis=1)
+print(data.sample(10))
+
