b7e5443fd6b8911bbbabbb4adc1b7be298ed62a8
Mohammad Fayzi <fayzi.dev@gmail.com>
2024-09-14 21:03:51 +0330

    Spliting the Dataset (Train , test )

--- Data_Preprocessing.py
+++ Data_Preprocessing.py
@@ -31,10 +31,10 @@ y = df['Purchased'].values
 
 
 #Solution 3 use scikit-learn
-from sklearn.impute import SimpleImputer
+# from sklearn.impute import SimpleImputer
 # imputer = SimpleImputer(missing_values =np.nan, strategy='median')
-imputer = SimpleImputer(missing_values =np.nan, strategy='mean')
-x[:, 1:3] = imputer.fit_transform(x[:, 1:3])
+# imputer = SimpleImputer(missing_values =np.nan, strategy='mean')
+# x[:, 1:3] = imputer.fit_transform(x[:, 1:3])
 # imputer.fit(x[:, 1:3])
 # x[:, 1:3] = imputer.transform(x[:, 1:3])
 # print(x)
@@ -48,11 +48,11 @@ from sklearn.preprocessing import OneHotEncoder
 col_trans = ColumnTransformer(transformers= [('Encoder', OneHotEncoder(), [0])],
                                remainder='passthrough')
 X = col_trans.fit_transform(x)
-print(X)
+# print(X)
 
 # OneHotEncoder by pandas get_dummies
-get_dum = pd.get_dummies(df,dtype=int)
-print(get_dum)
+# get_dum = pd.get_dummies(df,dtype=int)
+# print(get_dum)
 
 
 # Encoding the Dependent Variable
@@ -60,4 +60,15 @@ print(get_dum)
 from sklearn.preprocessing import LabelEncoder
 lable_Encode = LabelEncoder()
 Y = lable_Encode.fit_transform(y)
-print(Y)
+# print(Y)
+
+
+
+#Splitting the dataset into the Training set and Test set
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X,Y, test_size = 0.2, random_state = 1)
+print(X_train)
+print(X_test)
+print(y_train)
+print(y_test)
